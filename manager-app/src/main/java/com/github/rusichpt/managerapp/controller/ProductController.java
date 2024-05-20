27b41abff82e76213c4074d344fa62549a246ec6
tokarev_pa <tokarev_pa@tkbbank.ru>
2024-05-20 19:13:40 +0300

    Добавил обработку ошибок, интернализацию, валидацию данных

--- manager-app/src/main/java/com/github/rusichpt/managerapp/controller/ProductController.java
+++ manager-app/src/main/java/com/github/rusichpt/managerapp/controller/ProductController.java
@@ -3,19 +3,31 @@ package com.github.rusichpt.managerapp.controller;
 import com.github.rusichpt.managerapp.controller.payload.UpdateProductPayload;
 import com.github.rusichpt.managerapp.entity.Product;
 import com.github.rusichpt.managerapp.service.ProductService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
+import java.util.NoSuchElementException;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("catalogue/products/{productId:\\d+}")
 public class ProductController {
     private final ProductService productService;
+    private final MessageSource messageSource; // для интернационализации message.properties
 
     @ModelAttribute("product")
     public Product product(@PathVariable("productId") int productId) {
-        return productService.findProduct(productId).orElseThrow();
+        return productService.findProduct(productId)
+                .orElseThrow(() -> new NoSuchElementException("catalogue.errors.product.not_found"));
     }
 
     @GetMapping
@@ -29,15 +41,36 @@ public class ProductController {
     }
 
     @PostMapping("edit")
-    public String updateProduct(@ModelAttribute("product") Product product, UpdateProductPayload payload) {
-        productService.updateProduct(product.getId(), payload.title(), payload.details());
-        return "redirect:/catalogue/products/%d".formatted(product.getId());
+    public String updateProduct(@ModelAttribute(value = "product", binding = false) Product product,
+                                @Valid UpdateProductPayload payload,
+                                BindingResult bindingResult,
+                                Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("payload", payload);
+            model.addAttribute("errors", bindingResult.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage)
+                    .toList());
+            return "catalogue/products/edit";
+        } else {
+            productService.updateProduct(product.getId(), payload.title(), payload.details());
+            return "redirect:/catalogue/products/%d".formatted(product.getId());
+        }
     }
 
     @PostMapping("delete")
     public String deleteProduct(@ModelAttribute("product") Product product) {
         productService.deleteProduct(product.getId());
         return "redirect:/catalogue/products/list";
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public String handleNoSuchElementException(NoSuchElementException exception, Model model,
+                                               HttpServletResponse response, Locale locale) {
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        model.addAttribute("error",
+                messageSource.getMessage(exception.getMessage(), new Object[0],
+                        exception.getMessage(), locale));
+        return "errors/404";
     }
 
 }
--- manager-app/src/main/java/com/github/rusichpt/managerapp/controller/ProductsController.java
+++ manager-app/src/main/java/com/github/rusichpt/managerapp/controller/ProductsController.java
@@ -1,11 +1,14 @@
 package com.github.rusichpt.managerapp.controller;
 
-import com.github.rusichpt.managerapp.entity.Product;
 import com.github.rusichpt.managerapp.controller.payload.NewProductPayload;
+import com.github.rusichpt.managerapp.entity.Product;
 import com.github.rusichpt.managerapp.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,8 +31,18 @@ public class ProductsController {
     }
 
     @PostMapping("create")
-    public String createProduct(NewProductPayload payload) {
-        Product product = productService.createProduct(payload.title(), payload.details());
-        return "redirect:/catalogue/products/%d".formatted(product.getId());
+    public String createProduct(@Valid NewProductPayload payload,
+                                BindingResult bindingResult,
+                                Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("payload", payload);
+            model.addAttribute("errors", bindingResult.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage)
+                    .toList());
+            return "catalogue/products/new_product";
+        } else {
+            Product product = productService.createProduct(payload.title(), payload.details());
+            return "redirect:/catalogue/products/%d".formatted(product.getId());
+        }
     }
 }
